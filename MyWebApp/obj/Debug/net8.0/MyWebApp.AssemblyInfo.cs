0752f216fad714aebdacc2cf52a67f42b1f040ee
dleene <duy15904@gmail.com>
2025-03-16 22:32:37 +0700

    b39

--- MyWebApp/obj/Debug/net8.0/MyWebApp.AssemblyInfo.cs
+++ MyWebApp/obj/Debug/net8.0/MyWebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7008974486cc6f8e1bd273de4d9b7c333bbc9f0d")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
